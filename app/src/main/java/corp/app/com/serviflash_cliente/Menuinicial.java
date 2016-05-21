17f4e47f86e393caaeda51caaa9d10bc00377f29
farchenko90 <fandresroja@gmail.com>
2016-05-21 11:51:23 -0500

    icono menu

--- app/src/main/java/corp/app/com/serviflash_cliente/Menuinicial.java
+++ app/src/main/java/corp/app/com/serviflash_cliente/Menuinicial.java
@@ -1,7 +1,9 @@
 package corp.app.com.serviflash_cliente;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Menuinicial extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class Menuinicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menuinicial);
+
+        setToolbar();
+
     }
+
+    private void setToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            // Poner ícono del drawer toggle
+            ab.setHomeAsUpIndicator(R.drawable.ic_action_action_reorder_white);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
 }
